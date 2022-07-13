437b91d82abac01b26ad7e0660025f0d0fcc329c
William Liu <williamliu626@gmail.com>
2022-07-12 21:05:13 -0700

    finished day2 exercise

---
+++ 02_Day_Variables_builtin_functions/day2.py
@@ -0,0 +1,61 @@
+# 'Day 2: 30 Days of python programming'
+# Exercise 1
+
+first_name = 'Hello'
+last_name = 'World'
+full_name = 'Hello World'
+country = 'USA'
+city = 'Orange'
+age = 19
+year = 2022
+is_married = True
+is_true = True
+is_light_on = False
+this, becomes, multi, variable = 'this', 'is', 'interesting', 4000
+
+#Exercise 2
+# print(type(first_name))
+# print(type(last_name))
+# print(type(full_name))
+# print(type(country))
+# print(type(city))
+# print(type(age))
+# print(type(year))
+# print(type(is_married))
+# print(type(is_true))
+# print(type(is_light_on))
+
+print(len('William'))
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+diff = num_two - num_one
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+print(total)
+print(diff)
+print(product)
+print(division)
+print(remainder)
+print(exp)
+print(floor_division)
+
+# radius circle 30 m
+# calculate area and assign to area_of_circle
+# calculate circumference and assign to circum_of_circle
+# take radius as user input and calculate area
+radius_circle = 30
+area_of_circle = (radius_circle**2) * 3.14
+print(area_of_circle)
+circum_of_circle = 2 * radius_circle * 3.14
+print(circum_of_circle)
+radius = input("Please enter the radius: ")
+area = (radius ** 2) * 3.14
+print(area)
+
+
